e3d03565854d48b87aebce0ccd9add414a973590
Ashx321 <dashashsharma@gmail.com>
2023-11-10 20:47:07 +0530

    first commit

---
+++ src/pages/Cources.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cources = () => {
+    return (
+        <div>
+            <h1>Cources Page Here</h1>
+        </div>
+    );
+};
+
+export default Cources;
---
+++ src/pages/Tests.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tests = () => {
+    return (
+        <div>
+            <h1>Tests Page Here</h1>
+        </div>
+    );
+};
+
+export default Tests;
